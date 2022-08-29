0c3f257e3951f033a8a5e239e70955b5c92cb3cc
bishwajit1965 <paul.bishwajit09@gmail.com>
2022-08-29 13:50:15 +0600

    Committed

--- module-33-28-08-2022/various-meals-db-v__33-5/api/js/various-meal-db-v-33-5.js
+++ module-33-28-08-2022/various-meals-db-v__33-5/api/js/various-meal-db-v-33-5.js
@@ -11,7 +11,7 @@ const displayMeals = (meals) => {
     const mealDiv = document.createElement("col");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-    <div class="card">
+    <div onclick="loadMealsDetails(${meal.idMeal})" class="card">
       <img src="${meal.strMealThumb}" class="card-img-top" alt="Image" />
       <div class="card-body">
       <h3 class="card-title">Meal details</h3>
@@ -31,5 +31,35 @@ const searchFood = () => {
   const searchField = document.getElementById("search-field");
   const searchWord = searchField.value;
   loadMeals(searchWord);
+  searchWord = "";
+};
+
+const loadMealsDetails = (idMeal) => {
+  console.log("Button clicked", idMeal);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+  const mealDetails = document.getElementById("meal-details");
+  mealDetails.innerHTML = "";
+  mealDetails.classList.add("card");
+  mealDetails.innerHTML = `
+      <img src="${meal.strMealThumb}" class="card-img-top" alt="Image" />
+      <div class="card-body">
+      <h3 class="card-title">Meal details</h3>
+      <h6 class="card-title">Name: ${meal.strMeal}</h6>
+        <h6 class="card-title">Meal Id: ${meal.idMeal}</h6>
+        <h6 class="card-title">Desc: ${meal.strArea} ${meal.strCategory}</h6>
+        <p class="card-text">
+        Category: ${meal.strInstructions.slice(0, 100)}
+        </p>
+      </div>
+  `;
+  const mealCard = document.createElement("div");
+
+  mealDetails.appendChild(mealCard);
 };
 loadMeals("a");
