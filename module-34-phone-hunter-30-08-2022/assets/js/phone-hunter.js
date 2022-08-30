6bcde986201359493f5298220c4d4c3518bdbd88
bishwajit1965 <paul.bishwajit09@gmail.com>
2022-08-30 21:13:28 +0600

    Final commit

--- module-34-phone-hunter-30-08-2022/assets/js/phone-hunter.js
+++ module-34-phone-hunter-30-08-2022/assets/js/phone-hunter.js
@@ -35,8 +35,8 @@ const displayPhones = (phones, dataRange) => {
         <img src="${phone.image}" class="card-img-top" alt="Phone Image">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loadPhoneDetails">Show Details</button>
+            <p class="card-text">Click the button below to see the detailed features of the product. Wish you good luck.</p>
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#loadPhoneDetails">Show Details</button>
         </div>
     </div>`;
     phonesContainer.appendChild(phoneDiv);
